08d9660116c5e436e98a6c855867c05e19904168
Sepehr Raisi <sepehr.raisi@gmail.com>
2023-05-14 22:29:19 +0330

    Update

--- eshop_project/settings.py
+++ eshop_project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-import os
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -126,12 +126,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'statics'
+
 MEDIA_ROOT = BASE_DIR / 'uploads'
-MEDIA_URL = '/media/'
+MEDIA_URL = '/medias/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR / 'static'),
+    BASE_DIR / 'static'
 ]
 
 # Default primary key field type
@@ -141,6 +141,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # SESSION_COOKIE_AGE = 120
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'onionarchitecturemvc@gmail.com'
+EMAIL_HOST_PASSWORD = 'djangotoplearn@pass'
+EMAIL_PORT = 587
 
 # default settings
 JALALI_DATE_DEFAULTS = {
